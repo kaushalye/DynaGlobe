8ac708b44aa1a53a0500f2f5d606c0eaa3cc1eb1
Voon Siong Wong <vwong@dius.com.au>
2015-02-26 11:44:05 +1100

    Make datasets consistent

--- rgl/scratchpad.R
+++ rgl/scratchpad.R
@@ -1,14 +1,15 @@
 latlong2cart = function (coordinates) {
   R = 6371 # radius of earth in km
-  coordinates$x = R * cos(coordinates$latitude/180*pi) * cos(coordinates$longitude/180*pi)
-  coordinates$y = R * cos(coordinates$latitude/180*pi) * sin(coordinates$longitude/180*pi)
-  coordinates$z = R * sin(coordinates$latitude/180*pi)
+  coordinates$x = R * cos(coordinates$Latitude/180*pi) * cos(coordinates$Longitude/180*pi)
+  coordinates$y = R * cos(coordinates$Latitude/180*pi) * sin(coordinates$Longitude/180*pi)
+  coordinates$z = R * sin(coordinates$Latitude/180*pi)
   return(coordinates)
 }
 
-postcodes = read.csv('../data/au.csv')
-#postcodes = postcodes[c(1:10),]
-postcodes = latlong2cart(postcodes)
+#data = read.csv('../data/au.csv')
+data = read.csv('../GVP_Volcano_List.csv')
+
+data = latlong2cart(data)
 
 open3d()
-points3d(postcodes$x, postcodes$y, postcodes$z)
+points3d(data$x, data$y, data$z)
